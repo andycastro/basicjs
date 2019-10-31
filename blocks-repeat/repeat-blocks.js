a93c0cb3f600e79844b579caceaf49814dafddbd
Andy Castro <andycastro@MacBook-Pro-de-Andy.local>
2019-10-31 20:27:09 -0300

    chore: adds more samples functions js

---
+++ blocks-repeat/repeat-blocks.js
@@ -0,0 +1,28 @@
+// Repeat, repeat, repeat
+
+//Funções JavaScript
+
+function functionName () { // Funções literais
+  // acontece algo aqui
+}
+
+var varName = function () { // Funções anonimas
+  // acontece algo aqui
+}
+
+function functionName (param) { // Funções com parametros
+  // acontece algo aqui
+}
+
+function functionName () { // Função que retorna outra função como resultado
+  return function () {
+    // acontece algo aqui
+  }
+}
+
+function functionName () { // Funções aninhadas
+  function functionOtherName () {
+
+  }
+}
+
