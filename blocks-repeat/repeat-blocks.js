7cf48acdc8779e384a390ad6672f3aa4300c8148
Andy Castro <andycastro@MacBook-Pro-de-Andy.local>
2019-11-01 02:01:11 -0300

    chore: adds more samples functions js

--- blocks-repeat/repeat-blocks.js
+++ blocks-repeat/repeat-blocks.js
@@ -46,5 +46,64 @@ for ( i = 0; i < 5; i++ ){
   console.log(i);
 }
 
+// For in - percorre todas as propriedades de um objeto
+
+for ( variavel in objeto ) {
+  //faz alguma coisa
+}
+
 
 // While - Executa enquanto verdadeiro
+
+while (condicao){
+  //faz alguma coisa
+}
+
+// Condicionais
+
+// if
+
+if ( condicao ) {
+  //se verdadeiro, fará alguma coisa
+}
+
+// Ternário condicional JS
+
+condicao ? retornoSeTrue: retornoSeFalse;
+
+//ex
+
+let a = 10;
+
+a > 10 ? 'certo' : 'errado';
+
+// if else
+
+if ( condicao ) {
+  //se verdadeiro, fará alguma coisa
+} else{
+  //se falso, fará alguma coisa
+}
+
+// if else if - multiplas condicoes
+
+if ( condicao ){
+  // se verdadeiro, fará alguma coisa, caso contrário executará a próxima
+} else if ( condicao ){
+  // se verdadeiro fará alguma coisa, caso contrário executará a próxima
+} else {
+  // se falso, fará alguma coisa
+}
+
+// Operadores de comparação
+
+// ==    -> Igualdade de valor
+// ===   -> Igualdade de valor e tipo
+// !=     -> diferente em valor
+// !==    -> diferente em valor e tipo
+
+// Operadores lógicos
+
+// && - and ou e - retorna true - ex: isso && aquilo
+// || - or ou ou - retorna false - ex: isso || aquilo
+// !  - not ou negação - retorna true - ex: !arg 
