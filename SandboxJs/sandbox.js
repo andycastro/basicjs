b3e0ad4e43f5c421d675af4118d2cb9e34b0410b
Andy Castro <contato@andycastro.com.br>
2019-11-07 23:15:57 -0300

    feat: adds sandbox scripts

--- SandboxJs/sandbox.js
+++ SandboxJs/sandbox.js
@@ -1,11 +1,4 @@
-/* learning about arrays, indexOf() that verifies if content is on array, 
-its return 1 true and -1 false; ternary operator JS -> args ? foo : foo */
-
-function search(arr, item) {
-  const result = arr.indexOf(item) ? true : false;
-  
-  return result;
+function equalSlices(total, people, each) {
+	
 }
-console.log(search(
-  [1, 2, 3, 4, 5], 2
-));
+console.log(equalSlices(11, 5, 2));
