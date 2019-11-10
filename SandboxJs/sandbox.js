28684a95ab1643eb538e7322b60f2f52cb00c7dd
Andy Castro <contato@andycastro.com.br>
2019-11-10 01:04:03 -0300

    docs: update readme

--- SandboxJs/sandbox.js
+++ SandboxJs/sandbox.js
@@ -1,29 +1,9 @@
-/* Destructuring, Desestruturação JS ES6/ES7
+//THIS
+
+/*
+
+This se refere a um objeto dentro de um escopo.
+
+
+
 */
-carro = {
-  marca: 'Volkswagen',
-  modelo: 'Fusca',
-  opcionais: {
-    seguranca: {
-      itens: ['airbag', 'abs', 'green']
-    }
-  }
-};
-
-const { marca, opcionais: { seguranca: { itens }} } = carro;
-
-console.log(`Marca do veículo: ${marca}`);
-console.log(`Itens de segurança do veículo:  ${itens}`);
-
-// const companyName = company.name;
-// const shirtsColor = company.products.shirts.colors;
-
-// console.log(companyName);
-// console.log(shirtsColor);
-
-function imprimeDadosDoAluno(aluno) {
-  console.log(aluno.nome);
-  console.log(aluno.idade);
-  console.log(aluno.email);
-}
-imprimeDadosDoAluno("Matias");
